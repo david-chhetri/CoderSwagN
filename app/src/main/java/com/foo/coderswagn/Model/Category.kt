c5fe618d63787c85afe13dedcfad7887003be908
davidchhetri <david_chhetri@yahoo.com>
2021-10-04 13:37:06 -0400

    lesson01

---
+++ app/src/main/java/com/foo/coderswagn/Model/Category.kt
@@ -0,0 +1,4 @@
+package com.foo.coderswagn.Model
+
+class Category {
+}
---
+++ app/src/main/java/com/foo/coderswagn/Services/DataService.kt
@@ -0,0 +1,7 @@
+package com.foo.coderswagn.Services
+
+/**
+ * Created by David Chhetri on 04,October,2021
+ */
+object DataService {
+}
